e52dc8900f111c724d73f4a4f0d3cffabc233097
Michael Harry Setiawan <michael.mhs70@gmail.com>
2023-10-13 00:11:35 +0700

    express js installation + get json & database

--- server/script.js
+++ server/script.js
@@ -1,32 +1,62 @@
-const axios = require("axios")
-const useState = require("react")
-const useEffect = require("react")
+const axios = require("axios");
 const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
 
-const prisma = new PrismaClient({
-    datasourceUrl: env("DATABASE_URL")
-  })
-async function main() {
-    const [users, setUsers] = useState([]);
+const express = require('express')
+const app = express()
+const port = 5000
 
-    useEffect(() => {
-        axios.get(`http://localhost:8000/user`)
-            .then((res) => {
-                const fetchedUsers = res.data;
-                console.log(fetchedUsers)
-                setUsers(fetchedUsers);
-            });
-    }, []);
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-    const newUser = await prisma.user.create({
-        data: {
-            email: users.user.email,
-            name: users.user.name,
-            password: users.user.password,
-        },
+//fetch from json server
+app.get('/user', function (req, res) {
+  let users
+  axios.get(`http://localhost:8000/user`)
+    .then(response => {
+      users = response.data
+      res.send(users);
     })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send('An error occurred');
+    });
+})
 
-    const getUsers = await prisma.user.findMany()
-}
+//get from database
+app.get('/showuser', async (req, res) => {
+  const users = await prisma.user.findMany()
+  res.json(users)
+})
 
-main()
+app.get('/user/:id', function (req, res) {
+  res.send(req.params.id)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+// async function main() {
+//     await prisma.user.create({
+//       data: {
+//         name: 'Alice',
+//         email: 'alice@prisma.o',
+//         password: 'alice'
+//       },
+//     })
+  
+//     const allUsers = await prisma.user.findMany()
+//     console.dir(allUsers, { depth: null })
+//   }
+// main()
+//   .then(async () => {
+//     await prisma.$disconnect()
+//   })
+//   .catch(async (e) => {
+//     console.error(e)
+//     await prisma.$disconnect()
+//     process.exit(1)
+//   })
