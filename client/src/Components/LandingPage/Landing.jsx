74b4747d381591c793e7cf6ab01167253dd2692f
Michael Harry Setiawan <michael.mhs70@gmail.com>
2023-10-16 23:06:18 +0700

    bg-image

--- client/src/Components/LandingPage/Landing.jsx
+++ client/src/Components/LandingPage/Landing.jsx
@@ -9,7 +9,6 @@ import { createTheme, ThemeProvider, MuiCssBaseline } from '@mui/material/styles
 import CssBaseline from '@mui/material/CssBaseline';
 import BalooBhaijaan from '../../Font/BalooBhaijaan.ttf'
 import Paper from '@mui/material/Paper';
-import background from '../../Assets/landingPage_bg.png'
 
 const Landing = () => {
   const theme = createTheme({
@@ -30,9 +29,6 @@ const Landing = () => {
   return (
     <Container sx={{
       height:'607px',
-      background: `url(${background})`,
-      backgroundSize: 'cover',
-      marginLeft:'0px',
     }}>
       <Grid container spacing={5} sx={{marginTop : "0px"}}>
         <Grid item xs={8} >
