a149bcf7158c91628be71caaf108dfc24ebd7784
RichardTandean <115069181+RichardTandean@users.noreply.github.com>
2023-10-23 01:10:44 +0700

    device
    tabel

--- client/src/Components/LoginPage/Device.jsx
+++ client/src/Components/LoginPage/Device.jsx
@@ -1,6 +1,20 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios';
+import NavbarDashboard from './NavbarDashboard'
+import BalooBhaijaan from '../../Font/BalooBhaijaan.ttf'
+import Grid from '@mui/material/Unstable_Grid2'
+import { createTheme, ThemeProvider, MuiCssBaseline } from '@mui/material/styles';
+import Box from '@mui/material/Box'
+import CssBaseline from '@mui/material/CssBaseline';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { Container } from '@mui/system';
 
 export const Device = () => {
     const [plants, setPlants] = useState([]);
@@ -14,14 +28,63 @@ export const Device = () => {
                 setPlants(plants);
             });
     }, []);
+    const theme = createTheme({
+        typography: {
+          fontFamily: 'BalooBhaijaan',
+        },
+        components: {
+          MuiCssBaseline: {
+            styleOverrides: `
+              @font-face {
+                font-family: 'BalooBhaijaan';
+                src: url(${BalooBhaijaan});
+              }
+            `,
+          },
+        },
+      });
     return (
-        <div>
-            <h3>ID: {id}</h3>
-            <ul>
-                {plants.map((plant) => (
-                    <li key={plant.sensor_id}>Luminosity = {plant.luminosity} and Humidity = {plant.humidity}</li>
-                ))}
-            </ul>
+        <div style={{backgroundColor: '#C6D8C5', height:'720px'}}>
+            <NavbarDashboard />
+            <Container>
+                <Grid display="flex" justifyContent="center" alignItems="center">
+                    <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    <Box
+                        sx={{
+                        fontFamily: 'BalooBhaijaan',
+                        fontSize: '80px',
+                        height: '100px',
+                        color: 'Black',
+                        marginTop: '20px'
+                        }}
+                    >
+                        Device ID - {id}
+                    </Box>
+                    </ThemeProvider>
+                </Grid>
+                    <TableContainer component={Paper} sx={{marginTop:'50px'}}>
+                        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                            <TableHead>
+                                <TableRow>
+                                    {/* <TableCell>No</TableCell> */}
+                                    {/* <TableCell>Time</TableCell> */}
+                                    <TableCell align='center' sx={{fontWeight:'Bold'}}>Luminosity</TableCell>
+                                    <TableCell align='center' sx={{fontWeight:'Bold'}}>Humidty</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {plants.map((plant) => (
+                                    <TableRow key={plant.sensor_id}>
+                                        <TableCell align='center'>{plant.luminosity}</TableCell>
+                                        <TableCell align='center'>{plant.humidity}</TableCell>
+                                    </TableRow>
+
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+            </Container>
         </div>
     )
 }
